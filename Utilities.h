e552ce9cb2d12e2b10b815dcd262fa35274baaa0
Joshua Scoggins <Theoretically.x64@gmail.com>
2020-10-15 14:05:55 -0700

    Defined the pin function from the realmz decompilation efforts

--- Utilities.h
+++ Utilities.h
@@ -4,6 +4,7 @@
 
 #ifndef REALMZ_TOOLS_UTILITIES_H
 #define REALMZ_TOOLS_UTILITIES_H
+#include <type_traits>
 
 namespace realmz {
 
@@ -18,6 +19,27 @@ namespace realmz {
     template<typename T>
     constexpr bool twixt(T value, T minimum, T maximum) noexcept {
         return ((minimum <= value) && value <= maximum);
+    }
+
+    /**
+     * @brief Pin a given value into a provided range, if the value is less than min it is min, if it is greater than max then it is max. Optionally,
+     * you can cast the inputs to a different types before performing the comparisons itself
+     * @tparam T The type of value, min, and max
+     * @tparam CompareAs The type to cast value, min, and max to when performing the range comparisons
+     * @param value The value to check
+     * @param min  The smallest possible value
+     * @param max  The largest possible value
+     * @return The provided value pinned to the range specified
+     */
+    template<typename T, typename CompareAs = T>
+    constexpr T pin(T value, T min, T max) noexcept {
+        if (static_cast<CompareAs>(value) < static_cast<CompareAs>(min)) {
+            value = min;
+        }
+        if (static_cast<CompareAs>(max) < static_cast<CompareAs>(value)) {
+            value = max;
+        }
+        return value;
     }
 
 } // end namespace realmz
