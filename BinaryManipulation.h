0ec1f1ebb1ca01719cf8dbd8ce05c0de955b1f15
Joshua Scoggins <Theoretically.x64@gmail.com>
2020-10-19 14:40:29 -0700

    Fix up 32-bit make function

--- BinaryManipulation.h
+++ BinaryManipulation.h
@@ -7,10 +7,10 @@
 #include <cstdint>
 namespace realmz {
     constexpr uint16_t make(uint8_t first, uint8_t second) noexcept {
-        return ((static_cast<uint16_t>(first) << 8) & 0xFF00) | (static_cast<uint16_t>(second) & 0x00FF);
+        return ((static_cast<uint16_t>(first) << 8u) & 0xFF00) | (static_cast<uint16_t>(second) & 0x00FF);
     }
-    constexpr uint8_t upperHalf(uint16_t value) noexcept { return (value >> 8) & 0xFF; }
-    constexpr uint8_t lowerHalf(uint16_t value) noexcept { return value & 0xFF; }
+    constexpr uint8_t upperHalf(uint16_t value) noexcept { return (value >> 8u) & 0xFF; }
+    constexpr uint8_t lowerHalf(uint16_t value) noexcept { return static_cast<uint8_t>(value); }
 
     struct ConstructInt64 { constexpr ConstructInt64() = default; };
     struct ConstructInt32 { constexpr ConstructInt32() = default; };
@@ -23,15 +23,19 @@ namespace realmz {
                 0xFFFF'0000'0000'0000,
         };
         return (static_cast<uint64_t>(a) & Masks[0]) |
-               ((static_cast<uint64_t>(b) << 16) & Masks[1]) |
-               ((static_cast<uint64_t>(c) << 32) & Masks[2]) |
-               ((static_cast<uint64_t>(d) << 48) & Masks[3]);
+               ((static_cast<uint64_t>(b) << 16u) & Masks[1]) |
+               ((static_cast<uint64_t>(c) << 32u) & Masks[2]) |
+               ((static_cast<uint64_t>(d) << 48u) & Masks[3]);
     }
 
     static_assert(make(0x6789,0x2345,0xef01,0xabcd, ConstructInt64{}) == 0xABCDEF0123456789);
 
     constexpr uint32_t make(uint16_t a, uint16_t b, ConstructInt32) noexcept {
-        return (static_cast<uint32_t>(a) & 0xFFFF) | ((static_cast<uint32_t>(b) << 16) & 0xFFFF'0000);
+        constexpr uint32_t Masks[] {
+                0x0000'FFFF,
+                0xFFFF'0000,
+        };
+        return (static_cast<uint32_t>(a) & Masks[0]) | ((static_cast<uint32_t>(b) << 16) & Masks[1]);
     }
 
 }
