01d3c440c6d2ad6544bdf3a7f97cb2854ae3e607
Joshua Scoggins <Theoretically.x64@gmail.com>
2020-10-20 18:46:55 -0700

    Finished implementing the view aging logic :D

--- CharacterEditor/agegroupdisplayelement.cpp
+++ CharacterEditor/agegroupdisplayelement.cpp
@@ -25,4 +25,26 @@ AgeGroupDisplayElement::setAgeRange(const realmz::AgeRange& range) {
 
 void
 AgeGroupDisplayElement::setAgeModifier(const realmz::AgeModifiers& modifier) {
+    auto updateField = [](auto* element, int value) noexcept {
+        if (value == 0) {
+            element->clear();
+        } else {
+            element->setText(QString::number(value));
+        }
+    };
+    updateField(ui->agility, modifier.getAgility());
+    updateField(ui->brawn, modifier.getBrawn());
+    updateField(ui->charm, modifier.getCharm());
+    updateField(ui->chemical, modifier.getChemical());
+    updateField(ui->cold, modifier.getCold());
+    updateField(ui->electricalDrv, modifier.getElectrical());
+    updateField(ui->heat, modifier.getHeat());
+    updateField(ui->judgment, modifier.getJudgment());
+    updateField(ui->knowledge, modifier.getKnowledge());
+    updateField(ui->luck, modifier.getLuck());
+    updateField(ui->magicResist, modifier.getMagicResist());
+    updateField(ui->mental, modifier.getMental());
+    updateField(ui->movement, modifier.getMovement());
+    updateField(ui->magical, modifier.getMagical());
+    updateField(ui->vitality, modifier.getVitality());
 }
